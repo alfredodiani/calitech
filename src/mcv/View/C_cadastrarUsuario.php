9812e6cd84834f91b72e7f91b641667a7b206831
lilianasabato <liliana.teodoro@estudante.ufla.br>
2019-12-02 00:00:39 -0300

    comentários das alterações

---
+++ src/mcv/View/C_cadastrarUsuario.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = $_POST['cNome'];
+$nascimento = $_POST['cDataNascimento'];
+$rg= $_POST['cRg'];
+$email= $_POST['cEmail'];
+$senha= $_POST['cSenha'];
+
+echo "<html><body><h1>"
+.$nome."  ".$nascimento.
+	
+	"</h1> <body></html>"
+
+
+
+?>
+
