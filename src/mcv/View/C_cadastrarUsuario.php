04eee39d8769ea96d49ed3d873fc2fa43c305957
caiodinizufla <55419344+caiodinizufla@users.noreply.github.com>
2019-12-05 13:00:01 -0300

    Subindo telas (falta alteracao de passagem e funcao)

--- src/mcv/View/C_cadastrarUsuario.php
+++ src/mcv/View/C_cadastrarUsuario.php
@@ -1,38 +1,17 @@
 <?php
 
-//$idUsuario= $_POST['cUser'];
 $nome = $_POST['cNome'];
-$rg= $_POST['cRg'];
 $nascimento = $_POST['cDataNascimento'];
+$rg= $_POST['cRg'];
 $email= $_POST['cEmail'];
 $senha= $_POST['cSenha'];
 
 echo "<html><body><h1>"
 .$nome."  ".$nascimento.
 	
-	"</h1> <body></html>";
-
-$servidor= "localhost"; 
-$username= "root";
-$password= "";
-$bd= "usuario";
-	
-$conexaoInserir = mysqli_connect($servidor, $username, $password, $bd);
+	"</h1> <body></html>"
 
 
-	if(! $conexaoInserir){
-		die("conexao falhou ".$conexaoInserir->connect_error);
-	}
 
-
-$inserirSQL= "INSERT INTO usuario(nome, rg, nascimento, email, senha) VALUES ('$nome','$rg','$nascimento','$email','$senha')";
-
-if ( $conexaoInserir->query($inserirSQL) == TRUE){
-	echo "cliente salvo";
-}
-	else {
-		echo "falha <br>" .$conexaoInserir->error;
-	}
-	
 ?>
 
