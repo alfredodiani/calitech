d7a04531d8b642ea4819e1102acec342787567e8
lilianasabato <liliana.teodoro@estudante.ufla.br>
2019-12-04 23:48:33 -0300

    tela de cadastro com php

--- src/mcv/View/C_cadastrarUsuario.php
+++ src/mcv/View/C_cadastrarUsuario.php
@@ -1,17 +1,38 @@
 <?php
 
+//$idUsuario= $_POST['cUser'];
 $nome = $_POST['cNome'];
-$nascimento = $_POST['cDataNascimento'];
 $rg= $_POST['cRg'];
+$nascimento = $_POST['cDataNascimento'];
 $email= $_POST['cEmail'];
 $senha= $_POST['cSenha'];
 
 echo "<html><body><h1>"
 .$nome."  ".$nascimento.
 	
-	"</h1> <body></html>"
+	"</h1> <body></html>";
+
+$servidor= "localhost"; 
+$username= "root";
+$password= "";
+$bd= "usuario";
+	
+$conexaoInserir = mysqli_connect($servidor, $username, $password, $bd);
 
 
+	if(! $conexaoInserir){
+		die("conexao falhou ".$conexaoInserir->connect_error);
+	}
 
+
+$inserirSQL= "INSERT INTO usuario(nome, rg, nascimento, email, senha) VALUES ('$nome','$rg','$nascimento','$email','$senha')";
+
+if ( $conexaoInserir->query($inserirSQL) == TRUE){
+	echo "cliente salvo";
+}
+	else {
+		echo "falha <br>" .$conexaoInserir->error;
+	}
+	
 ?>
 
